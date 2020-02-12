d881fd373a4be8a62b75ae8398d492b9fc3ec0e5
treat-email <58233023+treat-email@users.noreply.github.com>
2020-02-12 21:32:10 +0200

    Update ValidationResult.php
    rewind stream content to avoid empty body on read

--- src/ValidationResult.php
+++ src/ValidationResult.php
@@ -24,6 +24,7 @@ final class ValidationResult
     {
         $this->checkResponseCode($response);
 
+        $response->getBody()->rewind();
         $json = $response->getBody()->getContents();
         $validationResult = \json_decode($json, true);
         if (\json_last_error() !== JSON_ERROR_NONE) {
@@ -44,6 +45,7 @@ final class ValidationResult
     {
         $this->checkResponseCode($response);
 
+        $response->getBody()->rewind();
         $json = $response->getBody()->getContents();
         $validationResult = \json_decode($json, true);
         if (\json_last_error() !== JSON_ERROR_NONE) {
